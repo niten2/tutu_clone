148fdfc8ad83e5684c36aa354b7c68dded452fe5
Niten <niten2@yandex.ru>
2015-10-03 11:45:29 +0700

    fix train compartment, reserved_seat

--- app/models/train.rb
+++ app/models/train.rb
@@ -5,4 +5,36 @@ class Train < ActiveRecord::Base
   has_many    :tickets
   has_many    :wagons
 
+  def reserved_seat
+    reserved_seat = []
+    self.wagons.each do |wagon|
+      if wagon.style == "плацкартный"
+        reserved_seat << wagon
+      end
+    end
+    return reserved_seat
+  end
+
+  def compartment
+    compartment = []
+    self.wagons.each do |wagon|
+      if wagon.style == "купейный"
+        compartment << wagon
+      end
+    end
+    return compartment
+  end
+
+
+# binding.pry
+
+
+
+# def count_lower_places
+#    # @train.wagons.each do |wagon|
+# end
+
+
+
+
 end
