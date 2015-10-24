6fb1d43fd067410ec82916bfe7989333080ad9b7
Niten <niten2@yandex.ru>
2015-10-24 16:06:43 +0700

    locale begin

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,11 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TutuExample
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/app/models/wagons)
     config.active_record.raise_in_transactional_callbacks = true
+    config.i18n.default_locale = :ru
   end
 end
