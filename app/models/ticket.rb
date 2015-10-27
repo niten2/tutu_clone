ec7a9878014d0174f5b6cdae0caca2bad53d1740
Niten <niten2@yandex.ru>
2015-10-27 12:25:58 +0700

    add mailer

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,8 +3,20 @@ class Ticket < ActiveRecord::Base
 
   belongs_to :train
   belongs_to :user
-
   belongs_to :starting_station, class_name: 'RailwayStation', foreign_key: :starting_station_id
   belongs_to :end_station, class_name: 'RailwayStation', foreign_key: :end_station_id
 
+  after_create :send_notification
+
+  def route_name
+    # binding.pry
+    "#{train.number}"
+    # "#{starting_station.name} - #{end_station.name}"
+  end
+
+  private
+
+  def send_notification
+    TicketsMailer.buy_ticket(self.user, self).deliver_now
+  end
 end
